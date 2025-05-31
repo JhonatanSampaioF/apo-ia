3e76e5c0f0031d90c4a787f8789ed99eafa6bb4b
Jhonatan Sampaio <jhonatan.sampaiof@gmail.com>
2025-05-31 18:52:38 -0300

    Second commit

---
+++ src/main/java/fiap/kciao/apo_ia/Application.java
@@ -0,0 +1,21 @@
+package fiap.kciao.apo_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	// TODO adicionar controller
+	// TODO adicionar login
+	// TODO adicionar autenticação
+	// TODO adicionar spring AI
+	// TODO rabbitMQ
+	// TODO testes unitários
+	// TODO testes de integração
+	// TODO mvc
+}
